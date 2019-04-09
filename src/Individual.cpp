54d5bed9315781985c754397291659f1ba719479
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-04-09 11:22:34 +0200

    Added overloaded constructor when clones_polys is null (do not include in argument list)

--- src/Individual.cpp
+++ src/Individual.cpp
@@ -7,12 +7,17 @@
 
 Individual::Individual(int number_of_polygons, int number_of_vertices, int max_x, int max_y,
                        vector<Polygon *> *cloned_polys) {
-    if (cloned_polys != nullptr) {
-        this->dna = cloned_polys;
-    } else {
-        for (int i = 0; i < number_of_polygons; ++i) {
-            this->dna->push_back(Polygon::random_polygon(number_of_polygons, max_x, max_y));
-        }
+    this->max_x = max_x;
+    this->max_y = max_y;
+    this->dna = cloned_polys;
+}
+
+
+Individual::Individual(int number_of_polygons, int number_of_vertices, int max_x, int max_y) {
+    this->max_x = max_x;
+    this->max_y = max_y;
+    for (int i = 0; i < number_of_polygons; ++i) {
+        this->dna->push_back(Polygon::random_polygon(number_of_polygons, max_x, max_y));
     }
 }
 
@@ -27,7 +32,19 @@ void Individual::mutate() {
     if (r < 0.45) {
         Colour *colour = poly->colour;
         if (r < 0.1125) {
-            colour.r = Colour::randomColour();
+            colour->set_r(Colour::random_colour());
+        } else if (r < 0.2250) {
+            colour->set_g(Colour::random_colour());
+        } else if (r < 0.3375) {
+            colour->set_b(Colour::random_colour());
+        } else {
+            colour->set_a(1 - utils::random());
+        }
+    } else if (r < 0.9) {
+        if (r < 0.675) {
+            poly->move(this->max_x, this->max_y);
+        } else {
+            Point point = poly
         }
     }
 }
